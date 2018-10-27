ba11c67146630920586e7f4d66bb8a8d499cc781
Ssaenz96 <saul_sesv@hotmail.xom>
2018-10-27 15:14:46 -0500

    Routes, Controllers and Seeders

---
+++ config/constants.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'seeders' => [
+        'tipos' => [
+        	0 => [
+        		'nombre' => 'Robo con arma blanca',
+        		'peso' => 10
+        	],
+
+        	1 => [
+        		'nombre' => 'Robo con arma de fuego',
+        		'peso' => 20
+        	],
+
+        	2 => [
+        		'nombre' => 'Carterista',
+        		'peso' => 5
+        	],
+
+        	3 => [
+        		'nombre' => 'Robo a vehiculo',
+        		'peso' => 15
+        	],
+
+        	4 => [
+        		'nombre' => 'Secuestro a menor de edad',
+        		'peso' => 50
+        	]
+        ]
+    ]
+];
